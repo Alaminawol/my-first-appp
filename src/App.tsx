d8d8db4f0286684d2f4969a271294e10d3023075
Alaminawol <alaminawol@gmail.com>
2026-02-19 08:14:52 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Todolist from './Components/todolist';
+
+
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <Todolist />
+      </div>
+      
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Todolist.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import { BiCheckDouble, BiRefresh, BiTrash, BiReset, BiEdit, BiCheckCircle} from 'react-icons/bi'
+import './todolist.css'
+
+function Todolist() {
+    const [todos, setTodos] = useState([])
+    const [inputvalue, setInputValue] = useState('')
+    const [editIndex, setEditIndex] = useState(-1)
+
+    const addTodo =() => {
+        if (inputvalue.trim() !== '') {
+           if (editIndex !== -1) {
+            const updatedTodos =[...todos]
+            updatedTodos[editIndex] = {task: inputvalue, completed: updatedTodos[editIndex].completed}
+            setTodos(updatedTodos)
+            setInputValue('')
+            setEditIndex(-1)
+           } else {
+            setTodos([...todos, {task: inputvalue, completed: false}])
+            setInputValue('')
+           }
+        }
+    }
+
+    const startEdit =(index) => {
+        setInputValue(todos[index].task)
+        setEditIndex(index)
+    }
+
+    const cancelEdit = () => {
+        setInputValue('')
+        setEditIndex(-1)
+    }
+
+    const removeTodo = (index) => {
+        const updatedTodos = todos.filter((_,i) => i !== index)
+        setTodos(updatedTodos)
+    }
+
+    const toggleCompleted = (index) => {
+        const updatedTodos = [...todos]
+        updatedTodos[index].completed = !updatedTodos[index].completed
+    }
+
+    useState
+  return (
+    <div className="todo-container">
+      <h1>to-do-listo</h1>
+      <div className='input-section'>
+        <input type="text" className="input-field" onChange={(e) => setInputValue(e.target.value)}
+           value={inputvalue}  placeholder='Enter a new Task' />
+
+        {editIndex !== -1 ? (
+
+        <>
+        <button onClick={addTodo} className='update-btn' >  <BiCheckDouble />  </button>
+        <button onClick={cancelEdit} className='clean-btn'>  <BiRefresh />  </button>
+        </>
+        ) : (
+        <button  onClick={addTodo} className='Add-btn'>  Add </button>
+        
+        )}
+      </div>
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+            <li key={index}> 
+                <span className="task-text">{todo.task}</span>
+                <div className="btn-group">
+                    <button onClick={() => startEdit(index)} className="btn-edit"> <BiEdit /> </button>
+                    <button onClick={() => removeTodo(index)} className="btn-remove"> <BiTrash /> </button>
+                    <button className="btn-done" onClick={() => toggleCompleted(index)}>
+                        {todo.completed ? <BiReset /> : <BiCheckCircle />}
+                    </button>
+                </div>
+            </li>
+        ))}
+
+
+      </ul>
+    </div>
+    
+  )
+}
+
+export default Todolist
